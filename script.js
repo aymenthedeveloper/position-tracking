d81c853d5321a76f5672c3e48cf35de549f82623
aymenthedeveloper <aymendev.contact@gmail.com>
2024-06-10 19:44:18 +0100

    finished the distance calculator using the gps and ready for testing

--- script.js
+++ script.js
@@ -1,25 +1,22 @@
 const start = document.querySelector('button.start');
 const end = document.querySelector('button.end');
-
-const i = { 'lan': 3.109384, 'lat': 36.737495 };
-const j = { 'lan': 3.104750, 'lat': 36.732378 };
 const path = [];
-const dist = 0;
 
-const toRadians = degrees => degrees * (Math.PI / 180);
-const getCoordinates = position => {
+
+function toRadians (degrees) {degrees * (Math.PI / 180);};
+function getCoordinates (position) {
   const {longitude, latitude} = position.coords;
   path.push({longitude, latitude})
 };
 
 function calculateDistance(p1, p2) {
   const R = 6371000 // m
-  const deltaPhi = toRadians(j.lat - i.lat);
-  const deltaLan = toRadians(j.lan - i.lan);
-  const a = Math.sin(deltaPhi / 2) ** 2 + Math.cos(toRadians(i.lat)) * Math.cos(
-    toRadians(j.lat)) * Math.sin(deltaLan / 2) ** 2;
+  const deltaPhi = toRadians(p2.latitude - p1.latitude);
+  const deltaLan = toRadians(p2.longitude - p1.longitude);
+  const a = Math.sin(deltaPhi / 2) ** 2 + Math.cos(toRadians(p1.latitude)) * Math.cos(
+    toRadians(p2.latitude)) * Math.sin(deltaLan / 2) ** 2;
   const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-  return (R * c).toFixed(4)
+  return +(R * c).toFixed(4) || 0;
 }
 
 function getPosition() {
@@ -33,23 +30,26 @@ let endTracking = false;
 function startTracking() {
   getPosition(); // to get starting point
   const tracking = setInterval(() => {
+    console.log(path[path.length - 1]);
     getPosition()
-    endTracking++
     if (endTracking){
       clearInterval(tracking)
-      const distance = path.reduce((a,b,i,arr) => {
+      const distance = path.slice().reduce((a,b,i,arr) => {
         if (arr[i+1] === undefined) return 0;
         return calculateDistance(arr[i],arr[i+1])
       });
-      dist = distance.reduce((a,b) => a+b);
-      console.log(dist);
+      console.log(distance);
     };
-  }, 4000)
+  }, 3000)
   return path
 };
 start.addEventListener('click', () => {
+  start.style.backgroundColor = '#222';
+  end.style.backgroundColor = 'red';
   startTracking();
 })
 end.addEventListener('click', () => {
+  start.style.backgroundColor = 'green';
+  end.style.backgroundColor = '#222';
   endTracking = true;
 })
